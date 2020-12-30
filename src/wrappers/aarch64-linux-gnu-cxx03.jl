ef33c720379d9f4399ff8f782ae5eb09cff1c14c
jlbuild <juliabuildbot@gmail.com>
2020-12-30 23:26:45 +0000

    CGAL_jll build 5.2.0+0

--- src/wrappers/aarch64-linux-gnu-cxx03.jl
+++ src/wrappers/aarch64-linux-gnu-cxx03.jl
@@ -5,94 +5,29 @@ using boost_jll
 using GMP_jll
 using MPFR_jll
 using Zlib_jll
-## Global variables
-PATH = ""
-LIBPATH = ""
-LIBPATH_env = "LD_LIBRARY_PATH"
-LIBPATH_default = ""
-
-# Relative path to `libCGAL`
-const libCGAL_splitpath = ["lib", "libCGAL.so"]
-
-# This will be filled out by __init__() for all products, as it must be done at runtime
-libCGAL_path = ""
-
-# libCGAL-specific global declaration
-# This will be filled out by __init__()
-libCGAL_handle = C_NULL
-
-# This must be `const` so that we can use it with `ccall()`
-const libCGAL = "libCGAL.so.13"
-
-
-# Relative path to `libCGAL_Core`
-const libCGAL_Core_splitpath = ["lib", "libCGAL_Core.so"]
-
-# This will be filled out by __init__() for all products, as it must be done at runtime
-libCGAL_Core_path = ""
-
-# libCGAL_Core-specific global declaration
-# This will be filled out by __init__()
-libCGAL_Core_handle = C_NULL
-
-# This must be `const` so that we can use it with `ccall()`
-const libCGAL_Core = "libCGAL_Core.so.13"
-
-
-# Relative path to `libCGAL_ImageIO`
-const libCGAL_ImageIO_splitpath = ["lib", "libCGAL_ImageIO.so"]
-
-# This will be filled out by __init__() for all products, as it must be done at runtime
-libCGAL_ImageIO_path = ""
-
-# libCGAL_ImageIO-specific global declaration
-# This will be filled out by __init__()
-libCGAL_ImageIO_handle = C_NULL
-
-# This must be `const` so that we can use it with `ccall()`
-const libCGAL_ImageIO = "libCGAL_ImageIO.so.14"
-
-
-"""
-Open all libraries
-"""
+JLLWrappers.@generate_wrapper_header("CGAL")
+JLLWrappers.@declare_library_product(libCGAL, "libCGAL.so.13")
+JLLWrappers.@declare_library_product(libCGAL_Core, "libCGAL_Core.so.13")
+JLLWrappers.@declare_library_product(libCGAL_ImageIO, "libCGAL_ImageIO.so.14")
 function __init__()
-    global artifact_dir = abspath(artifact"CGAL")
+    JLLWrappers.@generate_init_header(boost_jll, GMP_jll, MPFR_jll, Zlib_jll)
+    JLLWrappers.@init_library_product(
+        libCGAL,
+        "lib/libCGAL.so",
+        RTLD_LAZY | RTLD_DEEPBIND,
+    )
 
-    # Initialize PATH and LIBPATH environment variable listings
-    global PATH_list, LIBPATH_list
-    # From the list of our dependencies, generate a tuple of all the PATH and LIBPATH lists,
-    # then append them to our own.
-    foreach(p -> append!(PATH_list, p), (boost_jll.PATH_list, GMP_jll.PATH_list, MPFR_jll.PATH_list, Zlib_jll.PATH_list,))
-    foreach(p -> append!(LIBPATH_list, p), (boost_jll.LIBPATH_list, GMP_jll.LIBPATH_list, MPFR_jll.LIBPATH_list, Zlib_jll.LIBPATH_list,))
+    JLLWrappers.@init_library_product(
+        libCGAL_Core,
+        "lib/libCGAL_Core.so",
+        RTLD_LAZY | RTLD_DEEPBIND,
+    )
 
-    global libCGAL_path = normpath(joinpath(artifact_dir, libCGAL_splitpath...))
+    JLLWrappers.@init_library_product(
+        libCGAL_ImageIO,
+        "lib/libCGAL_ImageIO.so",
+        RTLD_LAZY | RTLD_DEEPBIND,
+    )
 
-    # Manually `dlopen()` this right now so that future invocations
-    # of `ccall` with its `SONAME` will find this path immediately.
-    global libCGAL_handle = dlopen(libCGAL_path)
-    push!(LIBPATH_list, dirname(libCGAL_path))
-
-    global libCGAL_Core_path = normpath(joinpath(artifact_dir, libCGAL_Core_splitpath...))
-
-    # Manually `dlopen()` this right now so that future invocations
-    # of `ccall` with its `SONAME` will find this path immediately.
-    global libCGAL_Core_handle = dlopen(libCGAL_Core_path)
-    push!(LIBPATH_list, dirname(libCGAL_Core_path))
-
-    global libCGAL_ImageIO_path = normpath(joinpath(artifact_dir, libCGAL_ImageIO_splitpath...))
-
-    # Manually `dlopen()` this right now so that future invocations
-    # of `ccall` with its `SONAME` will find this path immediately.
-    global libCGAL_ImageIO_handle = dlopen(libCGAL_ImageIO_path)
-    push!(LIBPATH_list, dirname(libCGAL_ImageIO_path))
-
-    # Filter out duplicate and empty entries in our PATH and LIBPATH entries
-    filter!(!isempty, unique!(PATH_list))
-    filter!(!isempty, unique!(LIBPATH_list))
-    global PATH = join(PATH_list, ':')
-    global LIBPATH = join(vcat(LIBPATH_list, [joinpath(Sys.BINDIR, Base.LIBDIR, "julia"), joinpath(Sys.BINDIR, Base.LIBDIR)]), ':')
-
-    
+    JLLWrappers.@generate_init_footer()
 end  # __init__()
-
--- src/wrappers/x86_64-w64-mingw32-cxx03.jl
+++ src/wrappers/x86_64-w64-mingw32-cxx03.jl
@@ -5,94 +5,29 @@ using boost_jll
 using GMP_jll
 using MPFR_jll
 using Zlib_jll
-## Global variables
-PATH = ""
-LIBPATH = ""
-LIBPATH_env = "PATH"
-LIBPATH_default = ""
-
-# Relative path to `libCGAL`
-const libCGAL_splitpath = ["bin", "libCGAL.dll"]
-
-# This will be filled out by __init__() for all products, as it must be done at runtime
-libCGAL_path = ""
-
-# libCGAL-specific global declaration
-# This will be filled out by __init__()
-libCGAL_handle = C_NULL
-
-# This must be `const` so that we can use it with `ccall()`
-const libCGAL = "libCGAL.dll"
-
-
-# Relative path to `libCGAL_Core`
-const libCGAL_Core_splitpath = ["bin", "libCGAL_Core.dll"]
-
-# This will be filled out by __init__() for all products, as it must be done at runtime
-libCGAL_Core_path = ""
-
-# libCGAL_Core-specific global declaration
-# This will be filled out by __init__()
-libCGAL_Core_handle = C_NULL
-
-# This must be `const` so that we can use it with `ccall()`
-const libCGAL_Core = "libCGAL_Core.dll"
-
-
-# Relative path to `libCGAL_ImageIO`
-const libCGAL_ImageIO_splitpath = ["bin", "libCGAL_ImageIO.dll"]
-
-# This will be filled out by __init__() for all products, as it must be done at runtime
-libCGAL_ImageIO_path = ""
-
-# libCGAL_ImageIO-specific global declaration
-# This will be filled out by __init__()
-libCGAL_ImageIO_handle = C_NULL
-
-# This must be `const` so that we can use it with `ccall()`
-const libCGAL_ImageIO = "libCGAL_ImageIO.dll"
-
-
-"""
-Open all libraries
-"""
+JLLWrappers.@generate_wrapper_header("CGAL")
+JLLWrappers.@declare_library_product(libCGAL, "libCGAL.dll")
+JLLWrappers.@declare_library_product(libCGAL_Core, "libCGAL_Core.dll")
+JLLWrappers.@declare_library_product(libCGAL_ImageIO, "libCGAL_ImageIO.dll")
 function __init__()
-    global artifact_dir = abspath(artifact"CGAL")
+    JLLWrappers.@generate_init_header(boost_jll, GMP_jll, MPFR_jll, Zlib_jll)
+    JLLWrappers.@init_library_product(
+        libCGAL,
+        "bin\\libCGAL.dll",
+        RTLD_LAZY | RTLD_DEEPBIND,
+    )
 
-    # Initialize PATH and LIBPATH environment variable listings
-    global PATH_list, LIBPATH_list
-    # From the list of our dependencies, generate a tuple of all the PATH and LIBPATH lists,
-    # then append them to our own.
-    foreach(p -> append!(PATH_list, p), (boost_jll.PATH_list, GMP_jll.PATH_list, MPFR_jll.PATH_list, Zlib_jll.PATH_list,))
-    foreach(p -> append!(LIBPATH_list, p), (boost_jll.LIBPATH_list, GMP_jll.LIBPATH_list, MPFR_jll.LIBPATH_list, Zlib_jll.LIBPATH_list,))
+    JLLWrappers.@init_library_product(
+        libCGAL_Core,
+        "bin\\libCGAL_Core.dll",
+        RTLD_LAZY | RTLD_DEEPBIND,
+    )
 
-    global libCGAL_path = normpath(joinpath(artifact_dir, libCGAL_splitpath...))
+    JLLWrappers.@init_library_product(
+        libCGAL_ImageIO,
+        "bin\\libCGAL_ImageIO.dll",
+        RTLD_LAZY | RTLD_DEEPBIND,
+    )
 
-    # Manually `dlopen()` this right now so that future invocations
-    # of `ccall` with its `SONAME` will find this path immediately.
-    global libCGAL_handle = dlopen(libCGAL_path)
-    push!(LIBPATH_list, dirname(libCGAL_path))
-
-    global libCGAL_Core_path = normpath(joinpath(artifact_dir, libCGAL_Core_splitpath...))
-
-    # Manually `dlopen()` this right now so that future invocations
-    # of `ccall` with its `SONAME` will find this path immediately.
-    global libCGAL_Core_handle = dlopen(libCGAL_Core_path)
-    push!(LIBPATH_list, dirname(libCGAL_Core_path))
-
-    global libCGAL_ImageIO_path = normpath(joinpath(artifact_dir, libCGAL_ImageIO_splitpath...))
-
-    # Manually `dlopen()` this right now so that future invocations
-    # of `ccall` with its `SONAME` will find this path immediately.
-    global libCGAL_ImageIO_handle = dlopen(libCGAL_ImageIO_path)
-    push!(LIBPATH_list, dirname(libCGAL_ImageIO_path))
-
-    # Filter out duplicate and empty entries in our PATH and LIBPATH entries
-    filter!(!isempty, unique!(PATH_list))
-    filter!(!isempty, unique!(LIBPATH_list))
-    global PATH = join(PATH_list, ';')
-    global LIBPATH = join(vcat(LIBPATH_list, [Sys.BINDIR, joinpath(Sys.BINDIR, Base.LIBDIR, "julia"), joinpath(Sys.BINDIR, Base.LIBDIR)]), ';')
-
-    
+    JLLWrappers.@generate_init_footer()
 end  # __init__()
-
